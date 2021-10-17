334957e5366e0ac8c5a18b94e90445d9e9a08972
C2H8N2 <73293479+Yaon-C2H8N2@users.noreply.github.com>
2021-10-17 16:52:12 +0200

    Add files via upload

--- tpInit/js/02_repereGUI.js
+++ tpInit/js/02_repereGUI.js
@@ -18,6 +18,8 @@ function init(){
  //plans contenant deux axes du repere
  //planRepere(scene);
 
+ //testCommit
+
  MaterialPhong = new THREE.MeshPhongMaterial({
    color: "#FF0000",
    opacity: 0.5,
