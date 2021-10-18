d1d8cf05367f7811d5eb5ccadca2f2934b29547d
C2H8N2 <y.dusoleil@outlook.com>
2021-10-18 14:02:10 +0200

    Update Pierre.js

--- tpInit/js/Pierre.js
+++ tpInit/js/Pierre.js
@@ -1,16 +1,16 @@
 function initPierre(scene,material){
   //traçage des différents cylindres
-  pierre = new THREE.Group();
+  let pierre = new THREE.Group();
 
-  nbFacesCylindres = 10;
-  poigne_horizontale = new THREE.CylinderGeometry(0.2,0.2,1,nbFacesCylindres);
-  mesh_poigne_horizontale = new THREE.Mesh(poigne_horizontale,material);
+  let nbFacesCylindres = 10;
+  let poigne_horizontale = new THREE.CylinderGeometry(0.2,0.2,1,nbFacesCylindres);
+  let mesh_poigne_horizontale = new THREE.Mesh(poigne_horizontale,material);
 
-  poigne_verticale = new THREE.CylinderGeometry(0.2,0.2,0.5,nbFacesCylindres);
-  mesh_poigne_verticale = new THREE.Mesh(poigne_verticale,material);
+  let poigne_verticale = new THREE.CylinderGeometry(0.2,0.2,0.5,nbFacesCylindres);
+  let mesh_poigne_verticale = new THREE.Mesh(poigne_verticale,material);
 
-  poigne_base = new THREE.CylinderGeometry(0.8,0.8,0.2,nbFacesCylindres*2);
-  mesh_poigne_base = new THREE.Mesh(poigne_base,material);
+  let poigne_base = new THREE.CylinderGeometry(0.8,0.8,0.2,nbFacesCylindres*2);
+  let mesh_poigne_base = new THREE.Mesh(poigne_base,material);
 
   //déplacement des cylindres aux positions souhaitées
   poigne_verticale.translate(0,0.35,0.6);
@@ -18,7 +18,7 @@ function initPierre(scene,material){
   poigne_horizontale.translate(0,0.8,-0.1);
 
   //merge des cylindres
-  poigne = new THREE.Geometry();
+  let poigne = new THREE.Geometry();
 
   mesh_poigne_base.updateMatrix();
   poigne.merge(mesh_poigne_base.geometry, mesh_poigne_base.matrix);
@@ -30,8 +30,8 @@ function initPierre(scene,material){
   poigne.merge(mesh_poigne_horizontale.geometry, mesh_poigne_horizontale.matrix);
 
   //création de la jointure des cylindres de la poignée
-  tabHaut = [];
-  tabBas = [];
+  let tabHaut = [];
+  let tabBas = [];
   for(i=0;i<nbFacesCylindres;i++){
     if(i<(nbFacesCylindres-1)/2){
       tabHaut.push(poigne_verticale.vertices[i]);
@@ -44,10 +44,10 @@ function initPierre(scene,material){
 
   //merge de la jointure au mesh final
   for(i=0;i<tabBas.length;i++){
-    faceGeom = new THREE.Geometry();
+    let faceGeom = new THREE.Geometry();
     faceGeom.vertices = [tabBas[i],tabHaut[i],tabHaut[(i+1)%tabHaut.length],tabBas[(i+1)%tabBas.length]];
     faceGeom.faces = [new THREE.Face3(0,1,2),new THREE.Face3(0,2,3)]
-    faceQuad = new THREE.Mesh(faceGeom);
+    let faceQuad = new THREE.Mesh(faceGeom);
     faceQuad.updateMatrix();
     poigne.merge(faceQuad.geometry, faceQuad.matrix);
   }
@@ -57,24 +57,31 @@ function initPierre(scene,material){
   pierre.add(mesh_poigne);
 
   //cylindre de la pierre
-  cylindre_milieu = new THREE.CylinderGeometry(1.5,1.5,0.5,nbFacesCylindres*2);
+  let cylindre_milieu = new THREE.CylinderGeometry(1.5,1.5,0.5,nbFacesCylindres*2);
   cylindre_milieu.translate(0,-1,0);
-  mesh_cylindre_milieu = new THREE.Mesh(cylindre_milieu,material);
+  let mesh_cylindre_milieu = new THREE.Mesh(cylindre_milieu,material);
   pierre.add(mesh_cylindre_milieu);
 
   //première lathe
-  p1 = poigne_base.vertices[nbFacesCylindres*2+nbFacesCylindres/2];
-  p3 = cylindre_milieu.vertices[nbFacesCylindres/2];
-  p2 = new THREE.Vector3(p3.x,p1.y,0);
+  let p1 = poigne_base.vertices[nbFacesCylindres*2+nbFacesCylindres/2];
+  let p3 = cylindre_milieu.vertices[nbFacesCylindres/2];
+  let p2 = new THREE.Vector3(p3.x,p1.y,0);
   pierre.add(latheBezTab(nbFacesCylindres,nbFacesCylindres*2,[p1,p2,p3],0x999999,1,false));
 
   //deuxième lathe
-  hauteur = Math.abs(p3.y-p1.y);
-  p4 = cylindre_milieu.vertices[nbFacesCylindres*2+nbFacesCylindres/2];
-  p5 = cylindre_milieu.vertices[nbFacesCylindres*2+nbFacesCylindres/2].clone();
+  let hauteur = Math.abs(p3.y-p1.y);
+  let p4 = cylindre_milieu.vertices[nbFacesCylindres*2+nbFacesCylindres/2];
+  let p5 = cylindre_milieu.vertices[nbFacesCylindres*2+nbFacesCylindres/2].clone();
   p5.y = p5.y-hauteur;
-  p6 = new THREE.Vector3(0,p5.y,0);
+  let p6 = new THREE.Vector3(0,p5.y,0);
   pierre.add(latheBezTab(nbFacesCylindres,nbFacesCylindres*2,[p4,p5,p6],0x999999,1,false));
 
-  scene.add(pierre);
+  pierre.scale.x = 0.2;
+  pierre.scale.y = 0.2;
+  pierre.scale.z = 0.2;
+  pierre.rotateX(Math.PI/2);
+  pierre.position.z = 0.35;
+  pierre.position.y = 0.5;
+
+  return pierre;
 }
