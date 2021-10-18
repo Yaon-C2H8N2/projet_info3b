4dc6549bfccbfd807f75bb681340d0a227e2c7e4
Corentin <corentin.diehl@gmail.com>
2021-10-18 14:00:41 +0200

    Update Piste.js

--- tpInit/js/Piste.js
+++ tpInit/js/Piste.js
@@ -1,16 +1,16 @@
 function initPiste(scene) {
-  fond = new THREE.PlaneGeometry(5,20,0);
+  fond = new THREE.PlaneGeometry(4.5,30,0);
   materialBlanc = new THREE.MeshBasicMaterial( {color: 0xd3d3d3, side: THREE.DoubleSide} );
   mesh_fond = new THREE.Mesh(fond, materialBlanc);
   //scene.add(mesh_fond);
 
-  rond_rouge = new THREE.RingGeometry( 0.5, 0.8, 20 );
+  rond_rouge = new THREE.RingGeometry( 1.88, 1.22, 20 );
   rond_rouge.translate(0,3,0.01);
   materialRouge = new THREE.MeshBasicMaterial( {color: 0xff0000, side: THREE.DoubleSide} );
   mesh_rond_rouge = new THREE.Mesh(rond_rouge, materialRouge);
   //scene.add(mesh_rond_rouge);
 
-  rond_bleu = new THREE.RingGeometry(0.1, 0.3, 20);
+  rond_bleu = new THREE.RingGeometry(0.66, 0.15, 20);
   rond_bleu.translate(0,3,0.01);
   materialBleu = new THREE.MeshBasicMaterial( {color: 0x0000ff, side: THREE.DoubleSide} );
   mesh_rond_bleu = new THREE.Mesh(rond_bleu, materialBleu);
