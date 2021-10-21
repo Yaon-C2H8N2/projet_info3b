4113a96ffcf95ee6e925f498608ee71c6a181889
Corentin <corentin.diehl@gmail.com>
2021-10-21 10:37:49 +0200

    Update Balais.js

--- tpInit/js/Balais.js
+++ tpInit/js/Balais.js
@@ -1,4 +1,4 @@
-function initBalai(scene) {
+/*function initBalai(scene) {
   manche = new THREE.CylinderGeometry(0.03, 0.03, 2, 50);
   mesh_manche = new THREE.Mesh(manche, MaterialPhong);
 
@@ -38,8 +38,38 @@ function initBalai(scene) {
   balai.merge(mesh_poil_balai3.geometry, mesh_poil_balai3.matrix);
 
   mesh_balai = new THREE.Mesh(balai, MaterialPhong);
-  
+
   scene.add(mesh_balai);
 
-}
+}*/
  //creation balai
+ MaterialPhong = new THREE.MeshPhongMaterial({
+   color: "#0000FF",
+   opacity: 0.5,
+   //transparent: true,
+   emissive: 0x000000,
+   //specular: "#00FFFF",
+   flatShading: true,
+   shininess: 30,
+   //wireframe: true,
+   side: THREE.DoubleSide,
+ })
+
+function initBalai(scene) {
+  //creation base Balai
+  baseBalai = new THREE.BoxGeometry(0.06/*largeur*/, 0.2/*longueur*/, 0.03/*eppaisseur*/)
+  meshBaseBalai = new THREE.Mesh(baseBalai, MaterialPhong);
+
+  //creation manche balai
+  mancheBalai = new THREE.CylinderGeometry(0.02, 0.01, 1.4, 10);
+  mancheBalai.translate(0,0.7,0);
+  meshMancheBalai = new THREE.Mesh(mancheBalai, MaterialPhong);
+  meshMancheBalai.rotation.x = Math.PI/2;
+  meshMancheBalai.rotation.z = -0.4;
+
+
+  balai = new THREE.Group();
+  balai.add(meshBaseBalai);
+  balai.add(meshMancheBalai)
+  return balai;
+}
