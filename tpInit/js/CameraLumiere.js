12def0953be73e14179d501ec89e1be855d1f7a2
C2H8N2 <y.dusoleil@outlook.com>
2021-10-26 10:05:18 +0200

    Update CameraLumiere.js

--- tpInit/js/CameraLumiere.js
+++ tpInit/js/CameraLumiere.js
@@ -9,85 +9,9 @@
   var zDir=0;//testZero(document.forms["controle"].DirZ.value);
   camera.position.set(xPos, yPos, zPos);
   camera.lookAt(xDir, yDir, zDir);
-  actuaPosCameraHTML(xPos, yPos, zPos,xDir, yDir, zDir);
     //camera.lookAt(scene.position);
     //camera.lookAt(new THREE.Vector3(0,0,0));
 } // fin fonction cameraLumiere
-
- function actuaPosCameraHTML(xPos, yPos, zPos,xDir, yDir, zDir){
-  document.forms["controle"].PosX.value=xPos;
-  document.forms["controle"].PosY.value=yPos;
-  document.forms["controle"].PosZ.value=zPos;
-  document.forms["controle"].DirX.value=xDir;
-  document.forms["controle"].DirY.value=yDir;
-  document.forms["controle"].DirZ.value=zDir;
- }
-
- function ajoutCameraGui(gui,menuGUI,camera){
-  // ajout de la camera dans le menu du GUI
- let guiCamera = gui.addFolder("Camera");
-  // ajout des propriete de la camera
- // actualisation de la camera apres clic sur la zone idoine
- // guiCamera.add(menuGUI,"cameraxPos",-borneVue,borneVue).onChange(posCamera());
- // guiCamera.add(menuGUI,"camerayPos",-borneVue,borneVue).onChange(posCamera());
- // guiCamera.add(menuGUI,"camerazPos",-borneVue,borneVue).onChange(posCamera());
-  // mouvement automatique mais il faut actualiser la direction et la position en meme temps
- // abscisse de la position de la camera dans le menu
- guiCamera.add(menuGUI,"cameraxPos",-borneVue,borneVue).onChange(function () {
-    camera.position.set(menuGUI.cameraxPos*testZero(menuGUI.cameraZoom), menuGUI.camerayPos*testZero(menuGUI.cameraZoom), menuGUI.camerazPos*testZero(menuGUI.cameraZoom));
-    // ecriture des proprietes de la camera dans html
-    actuaPosCameraHTML(menuGUI.cameraxPos, menuGUI.camerayPos, menuGUI.camerazPos,menuGUI.cameraxDir,menuGUI.camerayDir, menuGUI.camerazDir);
-    //document.forms["controle"].PosX.value=testZero(menuGUI.cameraxPos);
-    camera.lookAt(testZero(menuGUI.cameraxDir), testZero(menuGUI.camerayDir), testZero(menuGUI.camerazDir));
-  });
- // ordonnee de la position de la camera dans le menu
- guiCamera.add(menuGUI,"camerayPos",-borneVue,borneVue).onChange(function () {
-    camera.position.set(menuGUI.cameraxPos*testZero(menuGUI.cameraZoom), menuGUI.camerayPos*testZero(menuGUI.cameraZoom), menuGUI.camerazPos*testZero(menuGUI.cameraZoom));
-    // ecriture des proprietes de la camera dans html
-    actuaPosCameraHTML(menuGUI.cameraxPos, menuGUI.camerayPos, menuGUI.camerazPos,menuGUI.cameraxDir,menuGUI.camerayDir, menuGUI.camerazDir);
-    camera.lookAt(testZero(menuGUI.cameraxDir), testZero(menuGUI.camerayDir), testZero(menuGUI.camerazDir));
-  });
- // cote de la position de la camera dans le menu
- guiCamera.add(menuGUI,"camerazPos",-borneVue,borneVue).onChange(function () {
-    camera.position.set(menuGUI.cameraxPos*testZero(menuGUI.cameraZoom), menuGUI.camerayPos*testZero(menuGUI.cameraZoom), menuGUI.camerazPos*testZero(menuGUI.cameraZoom));
-    // ecriture des proprietes de la camera dans html
-    actuaPosCameraHTML(menuGUI.cameraxPos, menuGUI.camerayPos, menuGUI.camerazPos,menuGUI.cameraxDir,menuGUI.camerayDir, menuGUI.camerazDir);
-   // document.forms["controle"].PosX.value=testZero(menuGUI.cameraxPos);
-    camera.lookAt(testZero(menuGUI.cameraxDir), testZero(menuGUI.camerayDir), testZero(menuGUI.camerazDir));
-  });
-
- // zoom de la camera dans le menu
- guiCamera.add(menuGUI,"cameraZoom",-10,10).onChange(function () {
-    camera.position.set(menuGUI.cameraxPos*testZero(menuGUI.cameraZoom), menuGUI.camerayPos*testZero(menuGUI.cameraZoom), menuGUI.camerazPos*testZero(menuGUI.cameraZoom));
-    camera.lookAt(testZero(menuGUI.cameraxDir), testZero(menuGUI.camerayDir), testZero(menuGUI.camerazDir))
-  });
- // fin de la position de camera
- // direction de la camera
- // abscisse de la direction de la camera dans le menu
- guiCamera.add(menuGUI,"cameraxDir",-borneVue,borneVue).onChange(function () {
-    camera.position.set(menuGUI.cameraxPos*testZero(menuGUI.cameraZoom), menuGUI.camerayPos*testZero(menuGUI.cameraZoom), menuGUI.camerazPos*testZero(menuGUI.cameraZoom));
-    // ecriture des proprietes de la camera dans html
-    actuaPosCameraHTML(menuGUI.cameraxPos, menuGUI.camerayPos, menuGUI.camerazPos,menuGUI.cameraxDir,menuGUI.camerayDir, menuGUI.camerazDir);
-    camera.lookAt(testZero(menuGUI.cameraxDir), testZero(menuGUI.camerayDir), testZero(menuGUI.camerazDir))
-  });
- // ordonnee de la direction de la camera dans le menu
- guiCamera.add(menuGUI,"camerayDir",-borneVue,borneVue).onChange(function () {
-    camera.position.set(menuGUI.cameraxPos*testZero(menuGUI.cameraZoom), menuGUI.camerayPos*testZero(menuGUI.cameraZoom), menuGUI.camerazPos*testZero(menuGUI.cameraZoom));
-    // ecriture des proprietes de la camera dans html
-    actuaPosCameraHTML(menuGUI.cameraxPos, menuGUI.camerayPos, menuGUI.camerazPos,menuGUI.cameraxDir,menuGUI.camerayDir, menuGUI.camerazDir);
-    camera.lookAt(testZero(menuGUI.cameraxDir), testZero(menuGUI.camerayDir), testZero(menuGUI.camerazDir))
-  });
- // cote de la direction de la camera dans le menu
- guiCamera.add(menuGUI,"camerazDir",-borneVue,borneVue).onChange(function () {
-    camera.position.set(menuGUI.cameraxPos*testZero(menuGUI.cameraZoom), menuGUI.camerayPos*testZero(menuGUI.cameraZoom), menuGUI.camerazPos*testZero(menuGUI.cameraZoom));
-    // ecriture des proprietes de la camera dans html
-    actuaPosCameraHTML(menuGUI.cameraxPos, menuGUI.camerayPos, menuGUI.camerazPos,menuGUI.cameraxDir,menuGUI.camerayDir, menuGUI.camerazDir);
-    camera.lookAt(testZero(menuGUI.cameraxDir), testZero(menuGUI.camerayDir), testZero(menuGUI.camerazDir))
-  });
- /*guiCamera.add(menuGUI,"cameraxDir",-borneVue,borneVue).onChange(posCamera());
- guiCamera.add(menuGUI,"camerayDir",-borneVue,borneVue).onChange(posCamera());
- guiCamera.add(menuGUI,"camerazDir",-borneVue,borneVue).onChange(posCamera());*/
- }//fin fonction ajoutCameraGui
 //*************************************************************
 //*
 //        F I N     C A M E R A
